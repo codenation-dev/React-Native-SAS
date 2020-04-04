76584f4568355bb439a5b00a1ec1dd704e07a83c
Dev4ster <victormenezes35@hotmail.com>
2020-04-04 15:30:32 -0300

    Modulo 2

--- module-2/src/components/LookSteps/LookSteps.js
+++ module-2/src/components/LookSteps/LookSteps.js
@@ -11,23 +11,39 @@ import Steps from '../Steps/Steps';
 import Subtitle from '../Subtitle/Subtitle';
 import Header from '../Header/Header';
 import Content from '../Content/Content';
+import StepInstructions from '../StepInstructions';
 
 import {color} from '../../styles';
 
 import steps from '../../steps.json';
 
-const LookSteps = () => {
+const LookSteps = ({onFinish}) => {
+  const [stepBlocked, setStepBlocked] = React.useState(true);
+  const LockStep = () => setStepBlocked(true);
+  const unLockStep = () => setStepBlocked(false);
+
   return (
     <React.Fragment>
       <Header>
         <Subtitle color={color.light}>Bem-vindo ao programa</Subtitle>
       </Header>
       <Content>
-        <Steps steps={steps}>
+        <Steps
+          steps={steps}
+          onFinish={onFinish}
+          stepBlocked={stepBlocked}
+          LockStep={LockStep}>
           {({currentStep}) => (
             <Step key={currentStep.id}>
               <StepTitle>{currentStep.name}</StepTitle>
               <StepDescription>{currentStep.text}</StepDescription>
+              <StepInstructions
+                instructions={currentStep.instructions}
+                enableStep={() => {
+                  unLockStep();
+                  console.log(LockStep);
+                }}
+              />
             </Step>
           )}
         </Steps>
---
+++ module-2/src/components/StepInstructions/index.js
@@ -0,0 +1,95 @@
+import React from 'react';
+import {View, Text, FlatList, Switch, SafeAreaView} from 'react-native';
+import {colors} from '../../styles';
+
+const StepInstructions = ({instructions, enableStep}) => {
+  const [instructionsView, setInstructionsView] = React.useState(false);
+
+  const [inscructionsCheked, setInscructionsCheked] = React.useState({});
+
+  function handleShowInstructions() {
+    setInstructionsView(!instructionsView);
+  }
+
+  React.useEffect(() => {
+    const data = instructions.reduce((total, atual) => {
+      if (!total[atual.id]) {
+        total[atual.id] = false;
+      }
+      return total;
+    }, {});
+
+    setInscructionsCheked(data);
+  }, [instructions]);
+
+  React.useEffect(() => {
+    const allChecked = instructions.reduce((total, atual) => {
+      total = false;
+      if (inscructionsCheked[atual.id]) {
+        total = true;
+      }
+      return total;
+    }, false);
+
+    if (allChecked) {
+      console.log('liberar');
+      enableStep();
+    }
+  }, [instructions, inscructionsCheked, enableStep]);
+
+  function handleCheckInstruction(id) {
+    if (!inscructionsCheked[id]) {
+      const newInstructionData = {
+        ...inscructionsCheked,
+        [id]: true,
+      };
+      setInscructionsCheked(newInstructionData);
+    }
+  }
+
+  return (
+    <View>
+      <Text style={styles.H1Instructions} onPress={handleShowInstructions}>
+        {instructionsView ? 'Esconder Instruções' : 'Mostrar Instruções'}
+      </Text>
+      {instructionsView &&
+        instructions.map(item => (
+          <View key={item.id} style={styles.instruction}>
+            <Switch
+              trackColor={{false: '#767577', true: '#81b0ff'}}
+              value={inscructionsCheked[item.id]}
+              onValueChange={() => handleCheckInstruction(item.id)}
+              disabled={inscructionsCheked[item.id]}
+            />
+            <Text
+              style={[
+                inscructionsCheked[item.id] && styles.chekedInstructionText,
+                {flex: 1, flexWrap: 'wrap'},
+              ]}>
+              {item.text}
+            </Text>
+          </View>
+        ))}
+    </View>
+  );
+};
+
+const styles = {
+  H1Instructions: {
+    color: colors.primary,
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBotton: 10,
+    marginTop: 20,
+  },
+  instruction: {
+    marginBotton: 20,
+    padding: 5,
+    flexDirection: 'row',
+  },
+  chekedInstructionText: {
+    textDecorationLine: 'line-through',
+  },
+};
+
+export default StepInstructions;
