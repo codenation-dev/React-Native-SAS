18f0d00d2d76d40601adcf780aef9116ed8c4d27
Felipe Bueno <felipebueno181@gmail.com>
2020-04-04 15:19:58 -0300

    Entrega do desafio Module-2 React Native SAS

--- module-2/src/components/LookSteps/LookSteps.js
+++ module-2/src/components/LookSteps/LookSteps.js
@@ -2,11 +2,12 @@
  * @flow
  */
 
-import * as React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import Step from '../Step/Step';
 import StepTitle from '../StepTitle/StepTitle';
 import StepDescription from '../StepDescription/StepDescription';
+import StepInstructions from '../StepInstructions/StepInstructions';
 import Steps from '../Steps/Steps';
 import Subtitle from '../Subtitle/Subtitle';
 import Header from '../Header/Header';
@@ -15,8 +16,52 @@ import Content from '../Content/Content';
 import {color} from '../../styles';
 
 import steps from '../../steps.json';
+import {FlatList, Text} from 'react-native';
 
-const LookSteps = () => {
+const LookSteps = ( { onFinish } ) => {
+  const [showInstructions, setShowInstructions] = useState(false);
+  const [ checked, setChecked ] = useState([]);
+  console.disableYellowBox = true;
+
+  useEffect(() => {
+    let instructions = [];
+
+    steps.map(item => {
+      item.instructions.map(instruction => {
+        instruction.checked = false;
+        instructions.push(instruction);
+      })
+    })
+
+    setChecked(instructions);
+  }, [])
+
+  const handleCheck = id => {
+    let flag = true;
+    const instructions = checked.map(instruction => {
+
+      if( instruction.id == id ){
+          instruction.checked = true;
+      }
+
+      if( instruction.checked == false ) {
+          flag = false;
+      }
+
+      return instruction;
+    });
+
+    if(flag) {
+      onFinish();
+    }
+
+    setChecked( instructions );
+  }
+
+  const toggleInstructions = () => {
+    setShowInstructions(!showInstructions);
+  };
+
   return (
     <React.Fragment>
       <Header>
@@ -28,12 +73,44 @@ const LookSteps = () => {
             <Step key={currentStep.id}>
               <StepTitle>{currentStep.name}</StepTitle>
               <StepDescription>{currentStep.text}</StepDescription>
+              {/* hide */}
+              <Text
+                style={styles.instructionsLink}
+                onPress={toggleInstructions}>
+                {!showInstructions ? 'MOSTRAR' : 'ESCONDER'} INSTRUÇÕES
+              </Text>
+
+              <FlatList
+                style={
+                  showInstructions
+                    ? styles.instructionList
+                    : styles.instructionListHide
+                }
+                data={currentStep.instructions}
+                renderItem={({item}) => <StepInstructions item={item} handleCheck={ handleCheck } />}
+                keyExtractor={item => item.id}
+              />
             </Step>
           )}
         </Steps>
       </Content>
     </React.Fragment>
   );
+};
+
+const styles = {
+  instructionsLink: {
+    fontWeight: 'bold',
+    color: '#7159c1',
+    paddingTop: 10,
+    paddingLeft: 10,
+  },
+  instructionList: {
+    height: null,
+  },
+  instructionListHide: {
+    height: 0,
+  },
 };
 
 export default LookSteps;
