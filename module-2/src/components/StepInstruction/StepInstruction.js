91d218cfed3ea1f3b24cd585ddaa195c0c43c89d
Ziro VI <ahmad.forhat@gmail.com>
2020-04-05 20:37:03 -0300

    includes style

--- module-2/src/components/StepInstruction/StepInstruction.js
+++ module-2/src/components/StepInstruction/StepInstruction.js
@@ -30,3 +30,19 @@ const StepInstruction = ({children}) => {
     </View>
   );
 };
+
+const styles = {
+  instruction: {
+    flex: 1,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginTop: 15,
+  },
+  text: {
+    marginHorizontal: 10,
+    flex: 1,
+  },
+};
+
+export default StepInstruction;
