a13508a5fab700be9e542b07f1cb96bb84414fee
Ahmad Forhat <ahmad.forhat@gmail.com>
2020-04-18 15:06:49 -0300

    chore(modulo 4): new route pag ended

--- module-4/src/router/routers/OnboardingRouter.js
+++ module-4/src/router/routers/OnboardingRouter.js
@@ -6,9 +6,14 @@ import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
 
 import Welcome from '../../screens/Welcome/Welcome';
+import ended from '../../screens/Ended/Ended';
 import LookSteps from '../../screens/LookSteps/LookSteps';
 
-import {ONBOARDING_HOME, ONBOARDING_LOOK_STEPS} from '../routes';
+import {
+  ONBOARDING_HOME,
+  ONBOARDING_LOOK_STEPS,
+  ONBOARDING_ENDED,
+} from '../routes';
 
 const OnboardingStack = createStackNavigator();
 
@@ -18,9 +23,15 @@ const options = {
 
 const OnboardingRouter = () => {
   return (
-    <OnboardingStack.Navigator initialRouteName={ONBOARDING_HOME} screenOptions={options}>
+    <OnboardingStack.Navigator
+      initialRouteName={ONBOARDING_HOME}
+      screenOptions={options}>
       <OnboardingStack.Screen name={ONBOARDING_HOME} component={Welcome} />
-      <OnboardingStack.Screen name={ONBOARDING_LOOK_STEPS} component={LookSteps} />
+      <OnboardingStack.Screen name={ONBOARDING_ENDED} component={ended} />
+      <OnboardingStack.Screen
+        name={ONBOARDING_LOOK_STEPS}
+        component={LookSteps}
+      />
     </OnboardingStack.Navigator>
   );
 };
